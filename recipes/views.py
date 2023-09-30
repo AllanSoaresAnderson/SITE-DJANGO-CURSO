8f6b458bccc13b307b4ce91d53a9f42241412eaf
Allan Soares Anderson <allansoaresanderson@gmail.com>
2023-09-30 10:48:37 -0300

    28. Criando a header, separando um partial para head e usando include no template

--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request=request, template_name='global/home.html')
+    return render(request=request, template_name='recipes/pages/home.html')
