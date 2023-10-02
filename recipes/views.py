09172a2c991a9ce6905855e3c6b6f411cafe1456
Allan Soares Anderson <allansoaresanderson@gmail.com>
2023-10-02 20:40:19 -0300

    43. Separando os templates de home e recipe

--- recipes/views.py
+++ recipes/views.py
@@ -6,4 +6,4 @@ def home(request):
 
 
 def recipe(request, id):
-    return render(request=request, template_name='recipes/pages/home.html')
+    return render(request=request, template_name='recipes/pages/recipe-view.html')
